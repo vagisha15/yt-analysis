1aaecc3ad497c8a1b2651d222ee9845eaa0687c3
Thejas Bharadwaj <32273709+thejas98@users.noreply.github.com>
2023-01-31 13:50:43 -0500

    Create csv-to-parquet.py

---
+++ csv-to-parquet.py
@@ -0,0 +1,70 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# Script generated for node S3 bucket
+S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
+    format_options={
+        "quoteChar": '"',
+        "withHeader": True,
+        "separator": ",",
+        "optimizePerformance": False,
+    },
+    connection_type="s3",
+    format="csv",
+    connection_options={
+        "paths": ["s3://yt-analysis/youtube/raw_statistics/"],
+        "recurse": True,
+    },
+    transformation_ctx="S3bucket_node1",
+)
+
+# Script generated for node ApplyMapping
+ApplyMapping_node2 = ApplyMapping.apply(
+    frame=S3bucket_node1,
+    mappings=[
+        ("video_id", "string", "video_id", "string"),
+        ("trending_date", "string", "trending_date", "string"),
+        ("title", "string", "title", "string"),
+        ("channel_title", "string", "channel_title", "string"),
+        ("category_id", "bigint", "category_id", "bigint"),
+        ("publish_time", "string", "publish_time", "string"),
+        ("tags", "string", "tags", "string"),
+        ("views", "bigint", "views", "bigint"),
+        ("likes", "bigint", "likes", "bigint"),
+        ("dislikes", "bigint", "dislikes", "bigint"),
+        ("comment_count", "bigint", "comment_count", "bigint"),
+        ("thumbnail_link", "string", "thumbnail_link", "string"),
+        ("comments_disabled", "boolean", "comments_disabled", "boolean"),
+        ("ratings_disabled", "boolean", "ratings_disabled", "boolean"),
+        ("video_error_or_removed", "boolean", "video_error_or_removed", "boolean"),
+        ("description", "string", "description", "string"),
+    ],
+    transformation_ctx="ApplyMapping_node2",
+)
+
+# Script generated for node S3 bucket
+S3bucket_node3 = glueContext.getSink(
+    path="s3://yt-analysis-cleansed/youtube/",
+    connection_type="s3",
+    updateBehavior="UPDATE_IN_DATABASE",
+    partitionKeys=[],
+    enableUpdateCatalog=True,
+    transformation_ctx="S3bucket_node3",
+)
+S3bucket_node3.setCatalogInfo(
+    catalogDatabase="yt-analysis-cleansed", catalogTableName="csv-file-cleansed"
+)
+S3bucket_node3.setFormat("json")
+S3bucket_node3.writeFrame(ApplyMapping_node2)
+job.commit()
